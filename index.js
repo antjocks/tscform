c55ab43e8141018d6246a9e716da6178a91b543d
Anthony <antjocks@gmail.com>
2022-04-16 19:50:30 -0700

    Added error handling for form on client side and server side

--- index.js
+++ index.js
@@ -14,29 +14,38 @@ app.use(
     origin: ["https://champzendesk.herokuapp.com"],
   })
 );
+app.use(function(err, req, res, next) {
+  if(!err) return next();
+  console.log("Input error!");
+  res.send("Input error!");
+});
 
 app.get("/", function (req, res) {
   res.sendFile("index.html");
 });
 
 app.post("/createtickets", async (req, res) => {
-  const dataToSend = JSON.stringify(req.body);
-  const token = req.body.apiToken;
-
-  const zdResponse = await axios.post(
-    "https://clickup.zendesk.com/api/v2/tickets/create_many",
-    dataToSend,
-    {
-      headers: {
-        Authorization: "Bearer " + token,
-        "content-type": "application/json",
-      },
+    const dataToSend = JSON.stringify(req.body);
+    const token = req.body.apiToken;
+    try {
+    const zdResponse = await axios.post(
+        "https://clickup.zendesk.com/api/v2/tickets/create_many",
+        dataToSend,
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+            "content-type": "application/json",
+          },
+        }
+    );
+    console.log(zdResponse.data);
+    const responseWasSuccessful = zdResponse.status == 200 ? true : false;
+    const finalResponse = {success: responseWasSuccessful};
+    res.json(finalResponse)
+    } catch (error) {
+      res.json({success: false});
+      console.log("error");
     }
-  );
-  console.log(zdResponse.data);
-  const responseWasSuccessful = zdResponse.status == 200 ? true : false;
-  const finalResponse = { success: responseWasSuccessful };
-  res.json(finalResponse);
-});
+})
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
